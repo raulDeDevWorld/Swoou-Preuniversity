c8b89fc4016e2908368947581af7890bf76a995a
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-08-17 10:52:42 -0400

    DC

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  pwa: {
+    dest: 'public'
+  }
+})
+
